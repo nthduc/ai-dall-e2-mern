a6081fe8a961169ea8ed0ab9e153557b067cd692
nthduc <nguyenthaiduc0212@gmail.com>
2023-02-14 08:22:12 +0700

    :hammer: initial commit

---
+++ ai-dallet-e2/vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'url';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    preserveSymlinks: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+})
